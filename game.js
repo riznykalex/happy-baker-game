dbe9a9fba1f38c907e008169dcd15ef0819e7814
riznykalex <riznyk.alex.mail@gmail.com>
2026-08-04 15:38:12 +0300

    Фікс: центрування гри в вікні (flex замість autoCenter Phaser)

--- game.js
+++ game.js
@@ -1416,7 +1416,7 @@ const config = {
   scene: [BootScene, MenuScene, GameScene],
   scale: {
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
+    autoCenter: Phaser.Scale.NO_CENTER
   }
 };
 
